e6c7ba7da4a4877a968527706b6a2b0d8c91e326
0drikPil0n <2467457@cegepoutaouais.qc.ca>
2025-01-31 15:31:56 -0500

    Choix du genre
    J'ai ajouter la fonction qui permet de choisir le genre

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ def verifier_nom(p_prenom: str, p_nom: str) -> str:
     :return: Le nom complet
     """
     while not p_prenom.isalpha():
-        p_prenom = input("Entrer un prénom correcte: ").capitalize()
+        p_prenom = input("Entrer un prénom correcte: ").capitalize().strip()
     while not p_nom.isalpha():
-        p_nom = input("Entrer un nom de famille correcte: ").capitalize()
+        p_nom = input("Entrer un nom de famille correcte: ").capitalize().strip()
     p_nom_complet = p_prenom + " " + p_nom
 
     return p_nom_complet
@@ -33,12 +33,12 @@ def verifier_race(numero: int) -> dict:
     :return: La race choisis
     """
     while True:
-        if numero not in [1, 2, 3, 4]:
+        if numero not in range(1, len(races) + 1):
             print(f"\nEntrer un numero de 1 à {len(races)}\n")
             time.sleep(1)
             for p_position, p_race in enumerate(races):
                 print(f"{p_position + 1} - {p_race['Race']}")
-            numero: int = int(input("Veuillez choisir une race par son numéro: "))
+            numero: int = int(input("Veuillez choisir une race par son numéro: ").strip())
         else:
             break
     race_choisi = races[numero - 1]
@@ -46,16 +46,46 @@ def verifier_race(numero: int) -> dict:
     return race_choisi
 
 
+genres: list = ["Homme","Femme","Autre"]
+
+
+def verifier_genre(p_genre: str) -> str:
+    """
+    Vérifie si le genre choisis est correcte
+    :param p_genre: Le genre choisis
+    :return: Le genre choisis
+    """
+    while True:
+        if p_genre not in genres:
+            print("\nEntrer un genre correcte")
+            time.sleep(1)
+            p_genre = input("\nQuel est votre genre? (Homme/Femme/Autre): ").capitalize().strip()
+        else:
+            break
+    return p_genre
+
+
+classes:list = ["Royauté","Villageois","Magicien","Armée"]
+
+
 if __name__ == '__main__':
     # Choix du nom du personnage
-    prenom: str = input("Veuillez entrer le prénom de votre personnage: ").capitalize()
-    nom: str = input("Veuillez choisir votre nom de famille: ").capitalize()
+    prenom: str = input("Veuillez entrer le prénom de votre personnage: ").capitalize().strip()
+    nom: str = input("Veuillez choisir votre nom de famille: ").capitalize().strip()
     nom_complet = verifier_nom(prenom, nom)
 
     # Choix de la race du personnage
-    print(f"Voici les races disponibles:")
+    print(f"\nVoici les races disponibles:")
     for position, race in enumerate(races):
         print(f"{position + 1} - {race['Race']}")
-    num_race_choisi: int = int(input("Veuillez choisir une race par son numéro: "))
+    num_race_choisi: int = int(input("Veuillez choisir une race par son numéro: ").strip())
+    race = verifier_race(num_race_choisi)
+
+    # Choix du genre du personnage
+    genre: str = input("\nQuel est votre genre? (Homme/Femme/Autre): ").capitalize().strip()
+    genre = verifier_genre(genre)
+    print(f"\nVoici les races disponibles:")
+
+    # Choix de la classe du personnage
 
 
