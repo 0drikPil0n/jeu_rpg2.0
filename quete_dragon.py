ad06adf44c32329c243611462ace2a02596dcf1f
0drikPil0n <2467457@cegepoutaouais.qc.ca>
2025-02-08 18:19:47 -0800

    combat dragon finis

--- quete_dragon.py
+++ quete_dragon.py
@@ -12,27 +12,27 @@ def afficher_dragon():
     print("Dans cette mission, l'objectif est de traquer et de tuer un dragon redoutable qui\n"
           "terrorise un village voisin. Le dragon, qui vis dans une grotte profonde\n"
           "et obscure, est hostile et crache du feu sur quiconque ose pénétrer son domaine.\n")
-    time.sleep(10)
-    print("Les aventuriers doivent se préparer en rassemblant des armes. Après une des longues\n "
-          "semaines de préparation, vous décidez de vous mettre en marche vers cette grotte\n "
-          "hostile dans les montagnes. Une fois arriver à cette grotte, vous êtes incapable\n "
-          "de vaicre votre peur.\n")
-    time.sleep(10)
-    print("Des frissons parcours votre corps, vos dents se met à claquer,\n"
-          "vos jambes se met à trembler. C'est alors que vous prennez une grande inspiration et\n "
-          "vous penez votre courage à deux mains. En entrant dans la grotte une odeur nauséabonde \n"
-          "s'empare de votre corps, vous n'avez jamais senti quelque chose d'aussi mauvais.\n")
-    time.sleep(10)
-    print("C'est alors qu'un craquement se fait surgir comme quelqu'un qui marche sur une branche.\n "
-          "Vous décidez de baisser le regard et c'est à ce moment que vous remarquez que vous\n "
-          "marchez sur les cadavres de vos camarades qui eux aussi avaient essayé de vaincre se\n "
-          "fameux dragons. C'est alors qu'un bruit se fait entendre dans le fond de la grotte.\n")
-    time.sleep(10)
-    print("Au fur et à mesure que le bruit se rapproche, vous distinguez une silouhette énorme\n "
-          "se dirigeant vers vous. C'est alors que le dragon apparait et sans même vous laissez le temps\n"
-          "de réagir, vous envoies au sol d'un bref coup circulaire avec sa queue. Vous vous relever,\n "
-          "adrenaline dans le sang, vengeance dans l'esprit et vous vous élencer arme dans la main pour\n "
-          "peut être la dernière fois!\n")
+    # time.sleep(10)
+    # print("Les aventuriers doivent se préparer en rassemblant des armes. Après une des longues\n "
+    #       "semaines de préparation, vous décidez de vous mettre en marche vers cette grotte\n "
+    #       "hostile dans les montagnes. Une fois arriver à cette grotte, vous êtes incapable\n "
+    #       "de vaincre votre peur.\n")
+    # time.sleep(10)
+    # print("Des frissons parcours votre corps, vos dents se met à claquer,\n"
+    #       "vos jambes se met à trembler. C'est alors que vous prenez une grande inspiration et\n "
+    #       "vous prenez votre courage à deux mains. En entrant dans la grotte une odeur nauséabonde \n"
+    #       "s'empare de votre corps, vous n'avez jamais senti quelque chose d'aussi mauvais.\n")
+    # time.sleep(10)
+    # print("C'est alors qu'un craquement se fait surgir comme quelqu'un qui marche sur une branche.\n "
+    #       "Vous décidez de baisser le regard et c'est à ce moment que vous remarquez que vous\n "
+    #       "marchez sur les cadavres de vos camarades qui, eux aussi, avaient essayé de vaincre se\n "
+    #       "fameux dragons. C'est alors qu'un bruit se fait entendre dans le fond de la grotte.\n")
+    # time.sleep(10)
+    # print("Au fur et à mesure que le bruit se rapproche, vous distinguez une silhouette énorme\n "
+    #       "se dirigeant vers vous. C'est alors que le dragon apparait et sans même vous laissez le temps\n"
+    #       "de réagir, vous envoie au sol d'un bref coup circulaire avec sa queue. Vous vous relever,\n "
+    #       "adrenaline dans le sang, vengeance dans l'esprit et vous vous élancer arme dans la main pour\n "
+    #       "peut être la dernière fois!\n")
 
     return None
 
@@ -42,7 +42,7 @@ def choisir_decision_combat(stats: dict, role: str,p_tour:int) -> tuple[int | No
     Permet au joueur de choisir quoi faire avant le combat
     :param stats: Les statistiques du role choisis
     :param role: La sous-classe choisis
-    :param p_tour: Le nombre de tour avant de pouvoir utilisé l'attaque
+    :param p_tour: Le nombre de tours avant de pouvoir utiliser l'attaque
     :return: Les points de vie du joueur et du dragon
     """
     # Attaques
@@ -55,36 +55,37 @@ def choisir_decision_combat(stats: dict, role: str,p_tour:int) -> tuple[int | No
     # Choix combat
     if p_tour > 0:
         p_tour -= 1
-    decision = input(f"\nLe dragon se prépare à attaquer. Que voulez-vous faire?\n"
-          f"Attaquer (1) ou esquiver (2): ")
-    while decision not in ["1","2"]:
-        print(f"\nVeuillez choisir une action valide")
-        time.sleep(0.5)
+    while type(p_attaque) != int:
         decision = input(f"\nLe dragon se prépare à attaquer. Que voulez-vous faire?\n"
-                                  f"Attaquer (1) ou esquiver (2): ")
-    match decision:
-        case "1": # Le joueur attaque
-            choix_attaque = input(f"\nSouhaitez-vous faire une attaque normale ou une attaque spéciale?\n"
-              f"Attaque (1) ou attaque spéciale (2): ")
-            while choix_attaque not in ["1","2"]:
-                print(f"\nVeuillez sélectionner un choix valide")
-                time.sleep(0.5)
+              f"Attaquer (1) ou esquiver (2): ")
+        while decision not in ["1","2"]:
+            print(f"\nVeuillez choisir une action valide")
+            time.sleep(0.5)
+            decision = input(f"\nLe dragon se prépare à attaquer. Que voulez-vous faire?\n"
+                                f"Attaquer (1) ou esquiver (2): ")
+        match decision:
+            case "1": # Le joueur attaque
                 choix_attaque = input(f"\nSouhaitez-vous faire une attaque normale ou une attaque spéciale?\n"
-                                      f"Attaque (1) ou attaque spéciale (2): ")
-            match choix_attaque:
-                case "1":
-                    p_attaque = random.choice(attaques)
-                case "2":
-                    if p_tour != 0:
-                        print(f"\nVous ne pouvez pas utiliser votre attaque spéciale!\n"
-                              f"Attendez {p_tour} tour")
-                        time.sleep(1)
-                    else:
-                        p_attaque = random.choice(attaques_speciale)
-                        p_tour += 3
-        case "2": # Le joueur esquive
-            p_esquive = random.choice(chance_esquive)
-            p_attaque = 0
+                  f"Attaque (1) ou attaque spéciale (2): ")
+                while choix_attaque not in ["1","2"]:
+                    print(f"\nVeuillez sélectionner un choix valide")
+                    time.sleep(0.5)
+                    choix_attaque = input(f"\nSouhaitez-vous faire une attaque normale ou une attaque spéciale?\n"
+                                          f"Attaque (1) ou attaque spéciale (2): ")
+                match choix_attaque:
+                    case "1":
+                        p_attaque = random.choice(attaques)
+                    case "2":
+                        if p_tour != 0:
+                            print(f"\nVous ne pouvez pas utiliser votre attaque spéciale!\n"
+                                  f"Attendez {p_tour} tour")
+                            time.sleep(1)
+                        else:
+                            p_attaque = random.choice(attaques_speciale)
+                            p_tour += 3
+            case "2": # Le joueur esquive
+                p_esquive = random.choice(chance_esquive)
+                p_attaque = 0
     return p_attaque,p_esquive,p_tour
 
 
@@ -103,7 +104,7 @@ def combat_dragon(stats:dict, role:str, p_attaque:int, p_esquive:bool,pv_dragon:
     # Attaque dragon
     attaques_dragon = [120,90]
     chance_att_dragon = [True,False]
-    arme = stats[role]["Arme"] # Arme utilisé
+    arme = stats[role]["Arme"] # Arme utilisée
     if p_esquive:
         print("\nVous esquiver l'attaque du dragon")
     else:
@@ -127,6 +128,8 @@ def combat_dragon(stats:dict, role:str, p_attaque:int, p_esquive:bool,pv_dragon:
                 print(f"\nLe dragon attaque! Il vous inflige {attaque_dragon} dégats.")
                 pv_joueur -= attaque_dragon
                 time.sleep(1)
+                if pv_joueur > 0:
+                    print(f"Il vous reste {pv_joueur} points de vie")
             if not chance:
                 print(f"\nLe dragon à échouer son attaque!")
                 time.sleep(1)
