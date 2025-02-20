c19d18e95ba9f238382a8df37cb6fe53423d30cb
0drikPil0n <2467457@cegepoutaouais.qc.ca>
2025-02-19 18:11:13 -0800

    class Classe et début class Personnage

--- Création_personnage/classe.py
+++ Création_personnage/classe.py
@@ -1,2 +1,22 @@
+from Création_personnage.sous_classe import SousClasse
+
+
 class Classe:
-    def __init__(self, nom:str, ):
+    """
+    Catégorie de combattant (ex: Royauté, Magicien)
+    """
+
+    def __init__(self, nom: str, sous_classes: list[SousClasse]):
+        """
+        Initialize le nom ainsi que les sous-classes d'une classe
+        :param nom: Le nom de la classe
+        :param sous_classes: Les sous-classes
+        """
+        self._nom = nom
+        self._sous_classes = None
+
+    def get_sous_classes(self):
+        return self._sous_classes
+
+    def set_sous_classes(self, sous_classes):
+        self._sous_classes = sous_classes
--- Création_personnage/personnage.py
+++ Création_personnage/personnage.py
@@ -1,5 +1,24 @@
-from race import Race
+from Création_personnage.race import Race
+from Création_personnage.classe import Classe
+from Création_personnage.sous_classe import SousClasse
 
 
 class Personnage:
-    def __init__(self, nom:str, age:int, genre: str,race: Race):
+    """
+    Un avatar créé par le joueur
+    """
+    def __init__(self, nom:str, age:int, genre: str,race: Race, classe: Classe, sous_classe: SousClasse):
+        self._nom = nom
+        self._age = age
+        self._genre = genre
+        self._race = race
+        self._classe = classe
+        self._sous_classe = sous_classe
+
+
+    def enregistrer_personnage(self):
+        """
+        Enregistre le personnage dans le fichier de sauvegarde
+        :return:
+        """
+        with open
